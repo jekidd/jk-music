f811d263f183766aad85b20d122641433733644a
jordan kidd <jkidd@jimrohnprocess.com>
2024-07-02 19:05:05 -0400

    Update plugin description

--- jk-music.php
+++ jk-music.php
@@ -1,3 +1,8 @@
 /*
- * Plugin Name: JK Music Library
+Plugin Name:  JK Music Library
+Plugin URI:   https://github.com/jekidd/jk-music.git
+Description:  A plugin for creating and managing a WordPress library.
+Version:      1.0
+Author:       Jordan Kidd
+Author URI:   https://www.jekidd.com/
  */
